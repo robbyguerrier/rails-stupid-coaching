0747c3a531effc213b27534a98beef16bc4aa888
robby guerrier <robbyguerrier@gmail.com>
2017-08-07 15:27:23 +0200

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  # get 'coaching/answer'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    get 'answer', to: 'coaching#answer'
+    get 'ask', to: 'coaching#ask'
+
 end
